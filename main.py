e9cbe554c93731cc7f6186bac2b12dbe8cb37e1a
shubhamgore2468 <shubhamgore2468@gmail.com>
2024-08-14 18:29:07 -0700

    train model eval

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTraining
 from src.mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from src.mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from src.mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from src.mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+
 
 
 STAGE_NAME = "DATA INGESTION STAGE"
@@ -42,6 +44,16 @@ try:
    data_ingestion = ModelTrainerTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model Evaluation Stage"
+try:
+        logger.info(f"Starting {STAGE_NAME}")
+        obj = ModelEvaluationTrainingPipeline()
+        obj.main()
+        logger.info(f"Completed {STAGE_NAME}")
 except Exception as e:
         logger.exception(e)
         raise e
